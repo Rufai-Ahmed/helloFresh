bb63a8863a3925592fd09112567a275e3fc79b48
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-05-18 11:09:24 +0100

    few changes to folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Header from "@/app/static/Header";
 import React from "react";
-import Hero from "./pages/Hero";
-import Why from "./pages/Why";
-import Choice from "./pages/Choice";
-import What from "./pages/What";
-import Testimony from "./pages/Testimony";
-import Banner from "./pages/Banner";
-import Questions from "./pages/Questions";
+import Hero from "./_pages/Hero";
+import Why from "./_pages/Why";
+import Choice from "./_pages/Choice";
+import What from "./_pages/What";
+import Testimony from "./_pages/Testimony";
+import Banner from "./_pages/Banner";
+import Questions from "./_pages/Questions";
 import Footer from "./static/Footer";
 
 const page = () => {
